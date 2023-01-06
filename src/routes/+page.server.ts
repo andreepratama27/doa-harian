613aeb6e82768f141f13c109a037ad3b81217c75
andreepratama27 <andreepratama27@gmail.com>
2023-01-06 22:03:53 +0700

    fix: add exception on Promise

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ export const load = async () => {
   const response = await fetch('https://doa-doa-api-ahmadramadhan.fly.dev/api')
     .then(result => result.json())
     .then(data => data)
+    .catch(err => console.error(err))
 
   return {
     data: response
