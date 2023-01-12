056d2e4eff587ce2c1a98ac9da4bdc5af419717a
andreepratama27 <andreepratama27@gmail.com>
2023-01-12 18:22:55 +0700

    feat: add search functionality

--- src/components/SearchBar/SearchBar.svelte
+++ src/components/SearchBar/SearchBar.svelte
@@ -1,4 +1,34 @@
-<div class="w-full pb-8 mt-4 border-b-4 border-black search-wrapper">
+<script lang='ts'>
+	import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher()
+  export let searchString: string;
+
+  function handleChange(e) {
+    dispatch('search', {
+      text: e.target.value
+    })
+  }
+
+  function onClear() {
+    searchString = '';
+
+    dispatch('search', {
+      text: '',
+    })
+  }
+</script>
+
+<div class="w-full border-black search-wrapper relative">
   <p class="mb-4 font-semibold label">Pencarian</p>
-  <input type="text" name="query" placeholder="Cari Doa" class="w-full h-12 p-4 border-2 border-black outline-none">
+  <div class="flex items-center">
+    <input type="text" name="query" placeholder="Cari Doa" class="w-full h-12 p-4 border-2 border-black outline-none" on:keyup={handleChange} bind:value={searchString}>
+
+    {#if searchString?.length > 0}
+      <button class="absolute right-4 bg-black text-white px-4 py-0 flex justify-between w-24" on:click={onClear}>
+        <p>Clear</p>
+        &times;
+      </button>
+    {/if}
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,29 @@
   import type { PageData } from './$types';
 
   export let data: PageData;
+
+  let tempData = Object.assign({}, data);
+
+  function onSearch(event) {
+    const text = event.detail.text
+
+    if (text === '') {
+      data = tempData;
+    } else {
+      const filterData = data?.data.filter(x => x.doa.includes(text))
+      data = {
+        ...data,
+        data: filterData,
+      }
+    }
+  }
 </script>
 
-<div class="w-full py-8 prayer-lists">
+<div class="w-full min-h-screen py-8 prayer-lists bg-sand-500">
+  <SearchBar on:search={onSearch} />
+
+  <div class="my-8 bg-black h-1"></div>
+
   {#each data.data as item}
     <a href={`/doa/${item.id}`} class="flex items-center justify-between w-full p-4 mb-4 border-2 border-black lists hover:cursor-pointer hover:font-bold group bg-white">
       <p>{item.doa}</p>
